0ff6d7cd7ecadd3bd8190f2829e523566e1591f7
herdeybayor <shereefdeen4real@gmail.com>
2022-12-04 01:33:17 +0100

    chore: update home screen ♻

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,32 @@
-import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, SafeAreaView, StatusBar, Text, View } from "react-native";
 
 export default function HomeScreen() {
+    const navigation = useNavigation();
+
+    React.useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, [navigation]);
+
     return (
-        <View>
-            <Text className="text-red-500">
-                Open up App.js to start working on your app!
-            </Text>
+        <SafeAreaView>
+            {/* Header */}
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                <Image
+                    source={{ uri: "https://links.papareact.com/wru" }}
+                    className="h-7 aspect-square rounded-full p-4 bg-gray-300"
+                />
+                <View>
+                    <Text className="font-bold text-gray-400 text-xs">
+                        Deliver Now
+                    </Text>
+                    <Text className="font-bold text-xl">Current Location</Text>
+                </View>
+            </View>
             <StatusBar style="auto" />
-        </View>
+        </SafeAreaView>
     );
 }
