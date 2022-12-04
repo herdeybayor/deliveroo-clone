987dc6f962fa14713617792a15c8a2684332f8db
herdeybayor <shereefdeen4real@gmail.com>
2022-12-04 06:32:31 +0100

    feat: add categories 🎉

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Image, SafeAreaView, StatusBar, Text, View } from "react-native";
+import {
+    Image,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    Text,
+    TextInput,
+    View,
+} from "react-native";
+import * as Icons from "react-native-heroicons/outline";
+import { Categories } from "../components";
 
 export default function HomeScreen() {
     const navigation = useNavigation();
@@ -12,20 +22,50 @@ export default function HomeScreen() {
     }, [navigation]);
 
     return (
-        <SafeAreaView>
+        <SafeAreaView className="bg-white">
             {/* Header */}
-            <View className="flex-row pb-3 items-center mx-4 space-x-2">
+            <View className="flex-row py-3 items-center space-x-2 px-4">
                 <Image
                     source={{ uri: "https://links.papareact.com/wru" }}
                     className="h-7 aspect-square rounded-full p-4 bg-gray-300"
                 />
-                <View>
+                <View className="flex-1">
                     <Text className="font-bold text-gray-400 text-xs">
                         Deliver Now
                     </Text>
-                    <Text className="font-bold text-xl">Current Location</Text>
+                    <Text className="font-bold text-xl">
+                        Current Location
+                        <Icons.ChevronDownIcon size={20} color="#00CCBB" />
+                    </Text>
                 </View>
+                <Icons.UserIcon size={35} color="#00CCBB" />
             </View>
+
+            {/* Search */}
+            <View className="flex-row justify-between space-x-2 items-center pb-2 px-4">
+                <View className="bg-gray-200 flex-1 relative flex-row items-center">
+                    <Icons.MagnifyingGlassIcon
+                        className="absolute left-2"
+                        color="#777777"
+                        strokeWidth={2}
+                    />
+                    <TextInput
+                        className="pl-10 py-2 flex-1"
+                        placeholder="Restaurants and cuisine"
+                        keyboardType="default"
+                    ></TextInput>
+                </View>
+                <Icons.AdjustmentsHorizontalIcon color="#00CCBB" />
+            </View>
+
+            {/* Body */}
+            <ScrollView className="bg-gray-100">
+                {/* Categories */}
+                <Categories />
+
+                {/* Featured Rows */}
+            </ScrollView>
+
             <StatusBar style="auto" />
         </SafeAreaView>
     );
