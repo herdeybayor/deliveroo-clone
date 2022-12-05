15b8587216030cec69af4bc38f015b5b6908e0cf
herdeybayor <shereefdeen4real@gmail.com>
2022-12-05 15:04:25 +0100

    chore: update schemas ♻

--- cms/schemas/category.js
+++ cms/schemas/category.js
@@ -1,17 +1,18 @@
 export default {
-  name: 'category',
-  title: 'Category',
-  type: 'document',
-  fields: [
-    {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    },
-  ],
-}
+    name: "category",
+    title: "Menu Category",
+    type: "document",
+    fields: [
+        {
+            name: "name",
+            type: "string",
+            title: "Category Name",
+            validation: (Rule) => Rule.required(),
+        },
+        {
+            name: "image",
+            type: "image",
+            title: "Image of the Category",
+        },
+    ],
+};
